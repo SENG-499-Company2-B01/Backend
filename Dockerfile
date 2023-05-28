e0c74827bab671b20768ecbae86cf85bc91869cc
anthonyrodway <anthonyrodway@uvic.ca>
2023-05-28 11:27:22 -0700

    Completed user CRUD operation.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,22 @@
-#build stage
+# # build stage
+# FROM golang:alpine
+# RUN apk add --no-cache git
+# WORKDIR /go/src/app
+# COPY . .
+# EXPOSE 8000
+# RUN go get -d -v ./...
+# RUN go build -o /go/bin/app -v ./... 
+
+# CMD ["go","run","/go/src/app/main.go"]
+
+
+# build stage
 FROM golang:alpine
 RUN apk add --no-cache git
 WORKDIR /go/src/app
 COPY . .
 EXPOSE 8000
 RUN go get -d -v ./...
-RUN go build -o /go/bin/app -v ./... 
+RUN go build -o /go/bin/app
 
-CMD ["go","run","/go/src/app/main.go"]
+CMD ["/go/bin/app"]
--- main.go
+++ main.go
@@ -2,24 +2,53 @@ package main
 
 import (
 	"context"
-	// "encoding/json"
-	// "fmt"
 	"log"
+	"os"
 	"net/http"
+	"path/filepath"
 
+	"backend/modules/users"
+
+	"github.com/joho/godotenv"
 	"github.com/gorilla/mux"
-	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
-var collection *mongo.Collection
 
 func init() {
-	// Set up the MongoDB client
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// Get the current working directory
 	var err error
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error getting current working directory:", err)
+	}
+	log.Println("Current working directory:", dir)
+
+	// Construct the path to the .env file
+	envPath := filepath.Join(dir, ".env")
+
+	// Load the .env file
+	err = godotenv.Load(envPath)
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	// Load the environment variables locally
+	mongoUsername := os.Getenv("MONGO_USERNAME")
+	mongoPassword := os.Getenv("MONGO_PASSWORD")
+	mongoAddress := os.Getenv("MONGO_ADDRESS")
+	mongoPort := os.Getenv("MONGO_PORT")
+
+	// Set up the MongoDB client with SCRAM-SHA-1 authentication
+	clientOptions := options.Client().ApplyURI("mongodb://" + mongoAddress + ":" + mongoPort).
+		SetAuth(options.Credential{
+			Username:   mongoUsername,
+			Password:   mongoPassword,
+			AuthMechanism: "SCRAM-SHA-256",
+		})
+
 	client, err = mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
@@ -32,22 +61,52 @@ func init() {
 	}
 
 	log.Println("Connected to MongoDB!")
-
-	// Set the database and collection
-	database := client.Database("schedule_db")
-	collection = database.Collection("users")
 }
 
 func handleRequests() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/users", createUser).Methods(http.MethodPost)
-	router.HandleFunc("/users/{id}", getUser).Methods(http.MethodGet)
-	router.HandleFunc("/users/{id}", updateUser).Methods(http.MethodPut)
-	router.HandleFunc("/users/{id}", deleteUser).Methods(http.MethodDelete)
+	// // Example handle request
+	// router.HandleFunc("/", homePage).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// Users CRUD Operations
+	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		users.CreateUser(w, r, client.Database("schedule_db").Collection("users"))
+	}).Methods(http.MethodPost)
+
+	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		users.GetUsers(w, r, client.Database("schedule_db").Collection("users"))
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/users/{username}", func(w http.ResponseWriter, r *http.Request) {
+		users.GetUser(w, r, client.Database("schedule_db").Collection("users"))
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/users/{username}", func(w http.ResponseWriter, r *http.Request) {
+		users.UpdateUser(w, r, client.Database("schedule_db").Collection("users"))
+	}).Methods(http.MethodPut)
+
+	router.HandleFunc("/users/{username}", func(w http.ResponseWriter, r *http.Request) {
+		users.DeleteUser(w, r, client.Database("schedule_db").Collection("users"))
+	}).Methods(http.MethodDelete)
+
+	// Schedules CRUD Operations
+
+
+	// Courses CRUD Operations
+
+
+	// Classroom CRUD Operations
+
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
+
+// // Example Endpoint
+// func homePage(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Welcome to the HomePage!")
+// 	fmt.Println("Endpoint Hit: homePage")
+// }
 
 func main() {
 	handleRequests()
