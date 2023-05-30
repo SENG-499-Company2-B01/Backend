65590872fb652ff78828ef172e2244e3f93b1e63
NishchintDhawan <nishchint34@gmail.com>
2023-05-29 22:17:29 -0700

    Create and Read operations

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 	"net/http"
+	"os"
 	"path/filepath"
 
-	"backend/modules/users"
 	"backend/modules/schedules"
+	"backend/modules/users"
 
-	"github.com/joho/godotenv"
+	"backend/modules/classrooms"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -42,11 +44,13 @@ func init() {
 	mongoAddress := os.Getenv("MONGO_ADDRESS")
 	mongoPort := os.Getenv("MONGO_PORT")
 
+	log.Println("I am here")
+
 	// Set up the MongoDB client with SCRAM-SHA-1 authentication
 	clientOptions := options.Client().ApplyURI("mongodb://" + mongoAddress + ":" + mongoPort).
 		SetAuth(options.Credential{
-			Username:   mongoUsername,
-			Password:   mongoPassword,
+			Username:      mongoUsername,
+			Password:      mongoPassword,
 			AuthMechanism: "SCRAM-SHA-256",
 		})
 
@@ -114,9 +118,16 @@ func handleRequests() {
 
 	// Courses CRUD Operations
 
-
 	// Classroom CRUD Operations
 
+	// Classroom Post
+	router.HandleFunc("/classrooms", func(w http.ResponseWriter, r *http.Request) {
+		classrooms.CreateClassroom(w, r, client.Database("schedule_db").Collection("classrooms"))
+	}).Methods(http.MethodPost)
+
+	router.HandleFunc("/classrooms/{classroom}", func(w http.ResponseWriter, r *http.Request) {
+		classrooms.GetClassroom(w, r, client.Database("schedule_db").Collection("classrooms"))
+	}).Methods(http.MethodGet)
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
