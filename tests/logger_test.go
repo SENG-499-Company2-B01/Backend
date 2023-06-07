0b8a4596d48aa4f433dc0cdf522044af93e69592
anthonyrodway <anthonyrodway@uvic.ca>
2023-06-07 12:04:27 -0700

    Seperate testing function into individual tests

--- tests/logger_test.go
+++ tests/logger_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/SENG-499-Company2-B01/Backend/logger"
 )
 
-func TestLogger(t *testing.T) {
+func TestLoggerInfo(t *testing.T) {
 	// Redirect log output to a buffer for testing
 	var buf bytes.Buffer
 
@@ -27,8 +27,19 @@ func TestLogger(t *testing.T) {
 		t.Errorf("Info log message:\nExpected: %q\nGot: %q", expectedInfo, got)
 	}
 
+	// Clean up
+	log.SetOutput(os.Stderr)
+}
+
+func TestLoggerWarning(t *testing.T) {
+	// Redirect log output to a buffer for testing
+	var buf bytes.Buffer
+
+	// Initialize the logger with the buffer as the output
+	logger.InitLogger(&buf, &buf, &buf)
+
 	// Test Warning
-	expectedWarning := "\033[1;33m[WARNING]\033[0m logger_test.go:32 TestWarningMessage"
+	expectedWarning := "\033[1;33m[WARNING]\033[0m logger_test.go:43 TestWarningMessage"
 	logger.Warning("TestWarningMessage")
 
 	// Verify Warning log message
@@ -36,11 +47,22 @@ func TestLogger(t *testing.T) {
 		t.Errorf("Warning log message:\nExpected: %q\nGot: %q", expectedWarning, got)
 	}
 
+	// Clean up
+	log.SetOutput(os.Stderr)
+}
+
+func TestLoggerError(t *testing.T) {
+	// Redirect log output to a buffer for testing
+	var buf bytes.Buffer
+
+	// Initialize the logger with the buffer as the output
+	logger.InitLogger(&buf, &buf, &buf)
+
 	// Turn off the exit on error to test successfully
 	logger.SetExitOnError(false)
 
 	// Test Error
-	expectedError := "\033[1;31m[ERROR]\033[0m logger_test.go:43 TestErrorMessage"
+	expectedError := "\033[1;31m[ERROR]\033[0m logger_test.go:66 TestErrorMessage"
 	logger.Error(fmt.Errorf("TestErrorMessage"))
 
 	// Turn back on the exit on error 
@@ -50,6 +72,9 @@ func TestLogger(t *testing.T) {
 	if got := extractLogMessage(buf.String()); got != expectedError {
 		t.Errorf("Error log message:\nExpected: %q\nGot: %q", expectedError, got)
 	}
+
+	// Clean up
+	log.SetOutput(os.Stderr)
 }
 
 func extractLogMessage(logOutput string) string {
