b66b54560fb2385fc4bae22457b39bb788b5f793
anthonyrodway <anthonyrodway@uvic.ca>
2023-06-08 15:05:17 -0700

    Warning and Error logs now get written to logs/logs.txt file

--- logger/logger.go
+++ logger/logger.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"errors"
+	"time"
 )
 
 var (
@@ -16,14 +17,18 @@ var (
 	errorLogger   *log.Logger
 
 	// Flag to control program termination on error log
-	exitOnError = true
+	exitOnError = false//true
+
+	fileWriter io.Writer
 )
 
 // Initializes the loggers with their respective outputs and formats.
-func InitLogger(infoOutput, warningOutput, errorOutput io.Writer) error {
+func InitLogger(infoOutput, warningOutput, errorOutput, fileOutput io.Writer) error {
 	infoLogger = log.New(infoOutput, "", log.Ldate|log.Ltime)
 	warningLogger = log.New(warningOutput, "", log.Ldate|log.Ltime)
 	errorLogger = log.New(errorOutput, "", log.Ldate|log.Ltime)
+
+	fileWriter = fileOutput
 
 	// Check if any of the loggers failed to initialize
 	if infoLogger == nil || warningLogger == nil || errorLogger == nil {
@@ -49,7 +54,7 @@ func SetExitOnError(exit bool) {
 // Info logs an information message to the console with the file and line number.
 func Info(message string) {
 	// Print the information message to the console and colour the tag blue
-	infoLogger.Println("\033[1;34m[INFO]\033[0m", message)
+	infoLogger.Println("\033[1;34m[INFO]\033[0m     ", message)
 }
 
 // Warning logs a warning message to the console with the file and line number.
@@ -57,7 +62,12 @@ func Warning(message string) {
 	fileLine := getFileLine()
 
 	// Print the warning message to the console and colour the tag yellow
-	warningLogger.Println("\033[1;33m[WARNING]\033[0m", fileLine, message)
+	warningLogger.Println("\033[1;33m[WARNING]\033[0m  ", fileLine, "|", message)
+
+	// Write the warning message to the file
+	if fileWriter != nil {
+		fileWriter.Write([]byte(fmt.Sprintf("%s [WARNING]   %s | %s\n", getTimestamp(), fileLine, message)))
+	}
 }
 
 // Error logs an error message to the console with the file and line number.
@@ -66,9 +76,19 @@ func Error(err error) {
 	fileLine := getFileLine()
 
 	// Print the error to the console with the file and line number, and colour the tag red
-	errorLogger.Printf("\033[1;31m[ERROR]\033[0m %s %s", fileLine, err)
+	errorLogger.Printf("\033[1;31m[ERROR]\033[0m     %s | %s", fileLine, err)
+
+	// Write the error to the file
+	if fileWriter != nil {
+		fileWriter.Write([]byte(fmt.Sprintf("%s [ERROR]     %s | %s\n", getTimestamp(), fileLine, err)))
+	}
 
 	if exitOnError {
 		os.Exit(1)
 	}
+}
+
+// Returns the current timestamp in a specific format.
+func getTimestamp() string {
+	return time.Now().Format("2006/01/02 15:04:05")
 }
